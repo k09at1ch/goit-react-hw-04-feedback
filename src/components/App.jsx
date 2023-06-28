585938957bc94ab3c88d8c6d3963f7ff92c4e707
k09at1ch <117813113+k09at1ch@users.noreply.github.com>
2023-06-28 12:56:47 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
 import Section from './Section/Section';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
